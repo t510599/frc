0115800dbdee31519e9931b21e8ac50ada33f951
secminhr <secminhrian@gmail.com>
2019-06-10 19:52:57 +0800

    /api/train finished(probably)

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 from flask import *
 import cv2
+from api import *
 
 ft = cv2.freetype.createFreeType2()
 ft.loadFontData("./edukai-3.ttf", 0)
@@ -7,8 +8,23 @@ ft.loadFontData("./edukai-3.ttf", 0)
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+encodings = dict()
+
+
 @app.route('/')
 def index():
 	render_template('index.html')
 
 @app.route('/api/train', methods=['PUT'])
+	if not request.files:
+		return jsonify({'status': 'failed', 'error': 'no_file'}), 400
+	if not 'name' in request.form:
+		return jsonify({'status': 'failed', 'error': 'no_name'}), 400
+	file = list(request.files.values())[0]
+	name = request.form['name']
+	encoding = train(file)
+	encodings[name] = encoding
+	return jsonify({'status: succeed'})
+
+@app.route('/api/idnetify', methods=[''])
+

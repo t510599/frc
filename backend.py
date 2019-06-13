bc63a6f507eea4c6c404cf340fa444679eb515f5
secminhr <secminhrian@gmail.com>
2019-06-13 23:31:27 +0800

    update

--- backend.py
+++ backend.py
@@ -83,4 +83,4 @@ if __name__ == '__main__':
         pass
     # make sure singleton
     VideoCamera.get_camera()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, threaded=True)
